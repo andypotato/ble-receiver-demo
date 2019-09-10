4e71421f875eb20395decf95b759705f1a070b8a
andypotato <8491849+andypotato@users.noreply.github.com>
2019-09-10 11:29:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BLE Receiver (Peripheral) Demo
---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config = {
+
+  // Bluetooth UUID for controller service
+  controllerUUID: process.env.CONTROLLER_UUID,
+
+  // Controller number (0 = first)
+  controllerNum: process.env.CONTROLLER_NUM
+};
+
+module.exports = config;
